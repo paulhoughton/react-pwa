fd8e46deb2634dfdfd06ff37d6f6fd446b97957d
paulhoughton <paulhoughton@users.noreply.github.com>
2016-09-19 07:42:27 +0100

    React Router v4

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
-import { hashHistory } from 'react-router';
+import { Link } from 'react-router';
 
 export default () => (
   <Card shadow={0} style={{ textAlign: 'center' }}>
@@ -9,7 +9,7 @@ export default () => (
       About this Application
     </CardText>
     <CardActions border>
-      <Button colored onClick={() => hashHistory.push('/') } >OK</Button>
+      <Link to="/"><Button colored>OK</Button></Link>
     </CardActions>
   </Card>
 );
